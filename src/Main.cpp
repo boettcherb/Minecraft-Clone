0d820c403b6b5f9bf3bf68cc9166c077f610f8dd
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-05-27 23:43:37 -0500

    Create Main.cpp with a basic outline
    Create a main method and write the code to set up and initialize
    GLAD, GLFW, and OpenGL. Create a window and write a basic render
    loop to test that everything is working properly. A window with
    the given title and a single color background means that
    everything is working.

---
+++ src/Main.cpp
@@ -0,0 +1,57 @@
+#include <glad/glad.h>
+#include <GLFW/GLFW3.h>
+
+#include <iostream>
+#include <string>
+
+static unsigned int g_scrWidth = 800;
+static unsigned int g_scrHeight = 600;
+const char* WINDOW_TITLE = "OpenGL Window";
+
+int main() {
+    // initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // create the main window
+    GLFWwindow* window = glfwCreateWindow(g_scrWidth, g_scrHeight, WINDOW_TITLE, nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << '\n';
+    std::cout << "Starting Application...\n";
+
+    // set the clear color (background color)
+    glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+
+    // render loop
+    while (!glfwWindowShouldClose(window)) {
+        // clear the screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+    return 0;
+}
