0eba0848732e9d8b62b5d52868e41435d26d3085
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-05-28 01:10:02 -0500

    Create a Mesh class
    Create a class to encapsulate all the functionality of vertex
    buffers, vertex arrays, and index buffers. Each mesh contains only
    one vertex buffer, but can be divided into many submeshes (since
    one object can be made of multiple different materials or have many
    different colors). Each submesh has its own vertex array, index
    buffer, and shader.
    
    Use the Mesh class by passing in the vertex buffer data, the size
    (in bytes) of the data, and the layout of the data to the
    constructor. Then for each submesh pass in an index buffer, an
    index buffer count, and a shader. Finally, in the render loop
    call .render() once to render the entire mesh.
    
    Currently, this class trades control over the setup of vertex data
    for simplicity of use. This may change in the future.

---
+++ src/Mesh.cpp
@@ -0,0 +1,86 @@
+#include "Mesh.h"
+#include "ShaderProgram.h"
+
+#include <glad/glad.h>
+
+#include <vector>
+#include <numeric>
+
+Mesh::Submesh::Submesh(unsigned int vao, unsigned int ibo, unsigned int count, const ShaderProgram* shader)
+    : m_vertexArrayID{ vao }, m_indexBufferID{ ibo }, m_indexBufferCount{ count }, m_shader{ shader } {}
+
+Mesh::Mesh(const void* data, unsigned int size, const std::vector<unsigned int>& layout) 
+    : m_vbData{ data }, m_vbSize{ size }, m_vbLayout{ layout } {
+    glGenBuffers(1, &m_vbID);
+}
+
+Mesh::~Mesh() {
+    // delete submeshes
+    for (const Mesh::Submesh& mesh : m_meshes) {
+        glDeleteVertexArrays(1, &mesh.m_vertexArrayID);
+        glDeleteBuffers(1, &mesh.m_indexBufferID);
+    }
+    m_meshes.clear();
+
+    // delete vertex buffer
+    glDeleteBuffers(1, &m_vbID);
+}
+
+void Mesh::addSubmesh(const void* ibData, unsigned int count, const ShaderProgram* shader) {
+    // create and bind vertex array
+    unsigned int vertexArrayID;
+    glGenVertexArrays(1, &vertexArrayID);
+    glBindVertexArray(vertexArrayID);
+
+    // bind and set up vertex buffer
+    setVertexBuffer();
+
+    // create and bind index buffer
+    unsigned int indexBufferID = setIndexBuffer(ibData, count);
+
+    // unbind everything (vertex array first)
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    
+    m_meshes.emplace_back(vertexArrayID, indexBufferID, count, shader);
+}
+
+void Mesh::render() const {
+    for (const Mesh::Submesh& mesh : m_meshes) {
+        glBindVertexArray(mesh.m_vertexArrayID);
+        mesh.m_shader->bind();
+        glDrawElements(GL_TRIANGLES, mesh.m_indexBufferCount, GL_UNSIGNED_INT, nullptr);
+    }
+}
+
+void Mesh::setVertexBuffer() const {
+    // bind vertex buffer
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbID);
+
+    // pass in vertex data to OpenGL
+    glBufferData(GL_ARRAY_BUFFER, m_vbSize, m_vbData, GL_STATIC_DRAW);
+
+    // calculate the number of bytes of each vertex
+    // std::accumulate sums up the values in the vertex buffer layout
+    int stride = std::accumulate(m_vbLayout.begin(), m_vbLayout.end(), 0) * sizeof(float);
+
+    // tell openGL the layout of our vertex data.
+    unsigned long long offset = 0;
+    for (unsigned int i = 0; i < m_vbLayout.size(); ++i) {
+        glEnableVertexAttribArray(i);
+        // offset is the number of bytes from the start of the data, but OpenGL
+        // reads this information in as a const void pointer
+        const void* offsetPtr = reinterpret_cast<const void*>(offset);
+        glVertexAttribPointer(i, m_vbLayout[i], GL_FLOAT, false, stride, offsetPtr);
+        offset += m_vbLayout[i] * sizeof(float);
+    }
+}
+
+unsigned int Mesh::setIndexBuffer(const void* data, unsigned int count) const {
+    unsigned int indexBufferID;
+    glGenBuffers(1, &indexBufferID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW);
+    return indexBufferID;
+}
---
+++ src/Mesh.h
@@ -0,0 +1,36 @@
+#ifndef MESH_H_INCLUDED
+#define MESH_H_INCLUDED
+
+#include "ShaderProgram.h"
+
+#include <vector>
+
+class Mesh {
+
+	struct Submesh {
+		unsigned int m_vertexArrayID;
+		unsigned int m_indexBufferID;
+		unsigned int m_indexBufferCount;
+		const ShaderProgram* m_shader;
+		Submesh(unsigned int vao, unsigned int ibo, unsigned int count, const ShaderProgram* shader);
+	};
+
+	unsigned int m_vbID;
+	const void* m_vbData;
+	unsigned int m_vbSize;
+	std::vector<unsigned int> m_vbLayout;
+	std::vector<Mesh::Submesh> m_meshes;
+
+public:
+	Mesh(const void* data, unsigned int size, const std::vector<unsigned int>& layout);
+	~Mesh();
+
+	void addSubmesh(const void* ibData, unsigned int count, const ShaderProgram* shader);
+	void render() const;
+
+private:
+	void setVertexBuffer() const;
+	unsigned int setIndexBuffer(const void* data, unsigned int count) const;
+};
+
+#endif
