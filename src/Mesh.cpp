8a4564020af6f2d760ba7d5966e74c01da7b36e4
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-06-05 22:36:50 -0500

    Dynamically create new meshes
    Whenever the state of a chunk changes, a new mesh and vertex buffer
    will be created. This will allow all vertex buffers to be created
    with the exact amount of memory required and will allow the vertex
    buffers to be declared as static.
    
    This is done to optimize for memory instead of time. It normally
    takes less than 16 milliseconds (the time between each buffer swap
    with vsync enabled) to create and fill a vertex buffer. Ideas for
    the future include making only the chunks immediately around the
    player dynamic and all other chunks static, or creating and filling
    vertex buffers on a separate thread.

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -3,38 +3,26 @@
 
 #include <glad/glad.h>
 
-Mesh::Mesh(unsigned int vbSize) : m_vertexCount{ 0 } {
+Mesh::Mesh(const void* data, unsigned int size) : m_vertexCount{ size / sizeof(unsigned int) } {
+    // create and bind vertex array
     glGenVertexArrays(1, &m_vertexArrayID);
     glBindVertexArray(m_vertexArrayID);
-    m_vertexBufferID = setVertexBuffer(vbSize);
+
+    // create and bind vertex buffer
+    glGenBuffers(1, &m_vertexBufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, m_vertexBufferID);
+
+    // set up memory location for vertex data (don't pass in any data yet)
+    glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+
+    // tell openGL the layout of our vertex data
+    glEnableVertexAttribArray(0);
+    glVertexAttribIPointer(0, 1, GL_UNSIGNED_INT, sizeof(unsigned int), 0);
 }
 
 Mesh::~Mesh() {
     glDeleteVertexArrays(1, &m_vertexArrayID);
     glDeleteBuffers(1, &m_vertexBufferID);
-}
-
-unsigned int Mesh::setVertexBuffer(unsigned int size) const {
-    // create and bind vertex buffer
-    unsigned int vertexBufferID;
-    glGenBuffers(1, &vertexBufferID);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
-    
-    // set up memory location for vertex data (don't pass in any data yet)
-    glBufferData(GL_ARRAY_BUFFER, size, nullptr, GL_DYNAMIC_DRAW);
-
-    // tell openGL the layout of our vertex data.
-    glEnableVertexAttribArray(0);
-    glVertexAttribIPointer(0, 1, GL_UNSIGNED_INT, sizeof(unsigned int), 0);
-    
-    return vertexBufferID;
-}
-
-void Mesh::setVertexData(const void* data, unsigned int size) {
-    glBindVertexArray(m_vertexArrayID);
-    glBindBuffer(GL_ARRAY_BUFFER, m_vertexBufferID);
-    glBufferSubData(GL_ARRAY_BUFFER, 0, size, data);
-    m_vertexCount = size / sizeof(unsigned int);
 }
 
 void Mesh::render(const ShaderProgram* shader) const {
--- src/Mesh.h
+++ src/Mesh.h
@@ -3,22 +3,16 @@
 
 #include "ShaderProgram.h"
 
-#include <glm/glm.hpp>
-
 class Mesh {
     unsigned int m_vertexArrayID;
     unsigned int m_vertexBufferID;
     unsigned int m_vertexCount;
 
 public:
-    Mesh(unsigned int vbSize);
+    Mesh(const void* data, unsigned int vbSize);
     ~Mesh();
 
-    void setVertexData(const void* data, unsigned int size);
     void render(const ShaderProgram* shader) const;
-
-private:
-    unsigned int setVertexBuffer(unsigned int size) const;
 };
 
 #endif
