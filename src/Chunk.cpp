13419f228818ed7b3e5c05e5b14cf7c1ef4e03e0
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-06-10 16:12:37 -0500

    Redo the Chunk class with sections
    Split each chunk into 16 sections. Each section is 16x16x16 blocks
    and has its own mesh. Using sections will require only small
    portions of a chunk to be recreated whenever there is an update.
    Also, some sections may be entirely omitted if they are all air
    (usually over half the sections in a chunk are all air). This will
    save on memory since the block data in that section does not need
    to be stored.
    
    Chunk updates and omitting entire sections if they are all air
    still needs to be implemented.

--- src/Chunk.cpp
+++ src/Chunk.cpp
@@ -1,7 +1,6 @@
 #include "Chunk.h"
 #include "BlockInfo.h"
 #include "ShaderProgram.h"
-#include "Camera.h"
 #include "Mesh.h"
 
 #include <glm/glm.hpp>
@@ -10,17 +9,29 @@
 
 #include <new>
 
-Block::BlockType Chunk::BlockArray::get(int x, int y, int z) const {
-    return m_blockArray[x][y][z];
+#include <iostream>
+#include "Timer.h"
+
+void Chunk::Section::render(const ShaderProgram* shader) const {
+    m_mesh.render(shader);
 }
 
-void Chunk::BlockArray::put(int x, int y, int z, Block::BlockType type) {
-    m_blockArray[x][y][z] = type;
+Chunk::Chunk(float x, float z, ShaderProgram* shader) : m_posX{ x }, m_posZ{ z }, m_shader{ shader } {
+    for (int section = 0; section < NUM_SECTIONS; ++section) {
+        m_sections[section] = new Section();
+    }
+    generateTerrain();
+    for (int section = 0; section < NUM_SECTIONS; ++section) {
+        unsigned int* data = new unsigned int[BLOCKS_PER_SECTION * Block::VERTICES_PER_BLOCK];
+        unsigned int size = getVertexData(data, section);
+        if (size > 0) {
+            m_sections[section]->m_mesh.setVertexData(size, data);
+        }
+        delete[] data;
+    }
 }
 
-Chunk::Chunk(float x, float z, ShaderProgram* shader) : m_posX{ x }, m_posZ{ z }, 
-m_shader{ shader }, m_updated{ true }, m_mesh{ nullptr } {
-    m_blocks = new BlockArray;
+void Chunk::generateTerrain() {
     FastNoise noise;
     for (int X = 0; X < CHUNK_LENGTH; ++X) {
         for (int Z = 0; Z < CHUNK_WIDTH; ++Z) {
@@ -28,94 +39,97 @@ m_shader{ shader }, m_updated{ true }, m_mesh{ nullptr } {
             float noiseZ = Z + CHUNK_WIDTH * m_posZ;
             int groundHeight = static_cast<int>(50.0 + (noise.GetSimplexFractal(noiseX, noiseZ) + 1.0) / 2.0 * 30.0);
             for (int Y = 0; Y <= groundHeight - 4; ++Y) {
-                m_blocks->put(X, Y, Z, Block::BlockType::STONE);
+                put(X, Y, Z, Block::BlockType::STONE);
             }
-            m_blocks->put(X, groundHeight - 3, Z, Block::BlockType::DIRT);
-            m_blocks->put(X, groundHeight - 2, Z, Block::BlockType::DIRT);
-            m_blocks->put(X, groundHeight - 1, Z, Block::BlockType::DIRT);
-            m_blocks->put(X, groundHeight, Z, Block::BlockType::GRASS);
+            put(X, groundHeight - 3, Z, Block::BlockType::DIRT);
+            put(X, groundHeight - 2, Z, Block::BlockType::DIRT);
+            put(X, groundHeight - 1, Z, Block::BlockType::DIRT);
+            put(X, groundHeight, Z, Block::BlockType::GRASS);
             for (int Y = groundHeight + 1; Y < CHUNK_HEIGHT; ++Y) {
-                m_blocks->put(X, Y, Z, Block::BlockType::AIR);
+                put(X, Y, Z, Block::BlockType::AIR);
             }
         }
     }
 }
 
 Chunk::~Chunk() {
-    delete m_blocks;
-    delete m_mesh;
+    for (Section* section : m_sections) {
+        delete section;
+    }
 }
 
-unsigned int Chunk::getVertexData(unsigned int* data) {
+void Chunk::put(int x, int y, int z, Block::BlockType block) {
+    m_sections[y / SECTION_HEIGHT]->m_blocks[x][y & 0xF][z] = block;
+}
+
+Block::BlockType Chunk::get(int x, int y, int z) const {
+    return m_sections[y / SECTION_HEIGHT]->m_blocks[x][y & 0xF][z];
+}
+
+void Chunk::render(glm::mat4 viewMatrix, float zoom, float scrRatio) {
+    // send the MVP matrices to the shaders
+    glm::vec3 translation(m_posX * CHUNK_LENGTH, 0.0f, m_posZ * CHUNK_WIDTH);
+    m_shader->addUniformMat4f("u_model", glm::translate(glm::mat4(1.0f), translation));
+    m_shader->addUniformMat4f("u_view", viewMatrix);
+    glm::mat4 projection = glm::perspective(glm::radians(zoom), scrRatio, 0.1f, 300.0f);
+    m_shader->addUniformMat4f("u_projection", projection);
+
+    // render each section of the chunk
+    for (const Section* section : m_sections) {
+        section->render(m_shader);
+    }
+}
+
+unsigned int Chunk::getVertexData(unsigned int* data, int section) const {
     unsigned int* start = data;
     for (int x = 0; x < CHUNK_LENGTH; ++x) {
-        for (int y = 0; y < CHUNK_HEIGHT; ++y) {
+        int minY = section * SECTION_HEIGHT;
+        int maxY = (section + 1) * SECTION_HEIGHT;
+        for (int y = minY; y < maxY; ++y) {
             for (int z = 0; z < CHUNK_WIDTH; ++z) {
                 // skip if this block is air
-                if (m_blocks->get(x, y, z) == Block::BlockType::AIR) {
+                if (get(x, y, z) == Block::BlockType::AIR) {
                     continue;
                 }
                 // check each of the six sides to see if this block is adjacent to a transparent block
                 // only draw if this block is next to a transparent block or the edge of the chunk
-                if (x == CHUNK_LENGTH - 1 || Block::isTransparent(m_blocks->get(x + 1, y, z))) {
+                if (x == CHUNK_LENGTH - 1 || Block::isTransparent(get(x + 1, y, z))) {
                     setBlockFaceData(data, x, y, z, Block::BlockFace::PLUS_X);
                     data += Block::UINTS_PER_FACE;
                 }
-                if (x == 0 || Block::isTransparent(m_blocks->get(x - 1, y, z))) {
+                if (x == 0 || Block::isTransparent(get(x - 1, y, z))) {
                     setBlockFaceData(data, x, y, z, Block::BlockFace::MINUS_X);
                     data += Block::UINTS_PER_FACE;
                 }
-                if (y == CHUNK_HEIGHT - 1 || Block::isTransparent(m_blocks->get(x, y + 1, z))) {
+                if (y == CHUNK_HEIGHT - 1 || Block::isTransparent(get(x, y + 1, z))) {
                     setBlockFaceData(data, x, y, z, Block::BlockFace::PLUS_Y);
                     data += Block::UINTS_PER_FACE;
                 }
-                if (y == 0 || Block::isTransparent(m_blocks->get(x, y - 1, z))) {
+                if (y == 0 || Block::isTransparent(get(x, y - 1, z))) {
                     setBlockFaceData(data, x, y, z, Block::BlockFace::MINUS_Y);
                     data += Block::UINTS_PER_FACE;
                 }
-                if (z == CHUNK_WIDTH - 1 || Block::isTransparent(m_blocks->get(x, y, z + 1))) {
+                if (z == CHUNK_WIDTH - 1 || Block::isTransparent(get(x, y, z + 1))) {
                     setBlockFaceData(data, x, y, z, Block::BlockFace::PLUS_Z);
                     data += Block::UINTS_PER_FACE;
                 }
-                if (z == 0 || Block::isTransparent(m_blocks->get(x, y, z - 1))) {
+                if (z == 0 || Block::isTransparent(get(x, y, z - 1))) {
                     setBlockFaceData(data, x, y, z, Block::BlockFace::MINUS_Z);
                     data += Block::UINTS_PER_FACE;
                 }
             }
         }
     }
-    // all updates to vertex data were just processed
-    m_updated = false;
 
     // return the number of bytes that were initialized
-    return static_cast<unsigned int>(data - start) / Block::UINTS_PER_FACE * Block::BYTES_PER_FACE;
+    return static_cast<unsigned int>(data - start) * sizeof(unsigned int);
 }
 
 inline void Chunk::setBlockFaceData(unsigned int* data, int x, int y, int z, Block::BlockFace face) const {
-    const unsigned int* blockData = Block::getData(m_blocks->get(x, y, z), face);
+    const unsigned int* blockData = Block::getData(get(x, y, z), face);
     for (unsigned int vertex = 0; vertex < Block::VERTICES_PER_FACE; ++vertex) {
         // x pos takes bits 20-24, y takes bits 15-19, z takes bits 10-14 (from the right)
         // add the relative x, y, and z positions of the block in the chunk
         data[vertex] = blockData[vertex] + (x << 24) + (y << 15) + (z << 10);
     }
-}
-
-void Chunk::render(const Camera* camera, float scrRatio) {
-    if (m_updated) {
-        unsigned int* vbData = new unsigned int[MAX_VERTICES_PER_CHUNK];
-        unsigned int size = getVertexData(vbData);
-        if (m_mesh != nullptr) {
-            delete m_mesh;
-        }
-        m_mesh = new Mesh(vbData, size);
-        delete[] vbData;
-    }
-    float worldX = m_posX * CHUNK_LENGTH;
-    float worldZ = m_posZ * CHUNK_WIDTH;
-    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(worldX, 0.0f, worldZ));
-    m_shader->addUniformMat4f("u_model", model);
-    m_shader->addUniformMat4f("u_view", camera->getViewMatrix());
-    glm::mat4 projection = glm::perspective(glm::radians(camera->getZoom()), scrRatio, 0.1f, 300.0f);
-    m_shader->addUniformMat4f("u_projection", projection);
-    m_mesh->render(m_shader);
 }
--- src/Chunk.h
+++ src/Chunk.h
@@ -4,36 +4,39 @@
 #include "BlockInfo.h"
 #include "ShaderProgram.h"
 #include "Mesh.h"
-#include "Camera.h"
 
-inline constexpr unsigned int CHUNK_LENGTH = 16;  // x
-inline constexpr unsigned int CHUNK_HEIGHT = 256; // y
-inline constexpr unsigned int CHUNK_WIDTH  = 16;  // z
-inline constexpr unsigned int BLOCKS_PER_CHUNK = CHUNK_LENGTH * CHUNK_HEIGHT * CHUNK_WIDTH;
-inline constexpr unsigned int MAX_VERTICES_PER_CHUNK = BLOCKS_PER_CHUNK * Block::VERTICES_PER_BLOCK;
+inline constexpr int CHUNK_LENGTH = 16;  // x
+inline constexpr int CHUNK_HEIGHT = 256; // y
+inline constexpr int CHUNK_WIDTH = 16;  // z
+inline constexpr int BLOCKS_PER_CHUNK = CHUNK_LENGTH * CHUNK_HEIGHT * CHUNK_WIDTH;
+
+inline constexpr int NUM_SECTIONS = 16;
+inline constexpr int SECTION_HEIGHT = 16;
+inline constexpr int BLOCKS_PER_SECTION = BLOCKS_PER_CHUNK / NUM_SECTIONS;
 
 class Chunk {
 
-    struct BlockArray {
-        Block::BlockType m_blockArray[CHUNK_LENGTH][CHUNK_HEIGHT][CHUNK_WIDTH];
-        Block::BlockType get(int x, int y, int z) const;
-        void put(int x, int y, int z, Block::BlockType type);
+    struct Section {
+        Block::BlockType m_blocks[CHUNK_LENGTH][SECTION_HEIGHT][CHUNK_WIDTH];
+        Mesh m_mesh;
+        void render(const ShaderProgram* shader) const;
     };
 
     const float m_posX, m_posZ;
-    BlockArray* m_blocks;
-    Mesh* m_mesh;
+    Section* m_sections[NUM_SECTIONS];
     ShaderProgram* m_shader;
-    bool m_updated;
 
 public:
     Chunk(float x, float z, ShaderProgram* shader);
     ~Chunk();
 
-    unsigned int getVertexData(unsigned int* data);
-    void render(const Camera* camera, float scrRatio);
+    void put(int x, int y, int z, Block::BlockType block);
+    Block::BlockType get(int x, int y, int z) const;
+    void render(glm::mat4 viewMatrix, float zoom, float scrRatio);
+    unsigned int getVertexData(unsigned int* data, int section) const;
 
 private:
+    void generateTerrain();
     inline void setBlockFaceData(unsigned int* data, int x, int y, int z, Block::BlockFace face) const;
 };
 
